cec0aef4e7bc266be4fc7e9e249fc3a79f942bfd
jianren <365088870@qq.com>
2020-03-23 21:16:18 +0800

    05-导航栏数据完成

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,16 +3,30 @@ import indexCss from "./index.module.scss"
 import { Carousel } from 'antd-mobile';
 import { baseURL } from "../../utils/request"
 import axios from "axios"
+
+// 引入图片
+import nav1 from "../../assets/images/nav-1.png"
+import nav2 from "../../assets/images/nav-2.png"
+import nav3 from "../../assets/images/nav-3.png"
+import nav4 from "../../assets/images/nav-4.png"
+
 // import CityInput from "../../components/CityInput"
 
 class Index extends Component {
     state = {
-        // 轮播图数据
-        carouselList: []
+        carouselList: [], // 轮播图数据
+        navs: [
+            { id: 0, text: "整租", imgSrc: nav1 },
+            { id: 1, text: "合租", imgSrc: nav2 },
+            { id: 2, text: "地图找房", imgSrc: nav3 },
+            { id: 3, text: "去出租", imgSrc: nav4 }
+        ] //导航栏数据
     }
     async componentDidMount() {
         // 获取轮播图数据
         this.getSwiper();
+        // 获取导航栏数据
+        this.getGroup()
     }
 
     getSwiper = async () => {
@@ -20,6 +34,12 @@ class Index extends Component {
         const res = await axios.get("/home/swiper")
         // console.log(res);
         this.setState({ carouselList: res.data.body })
+    }
+
+    getGroup = async () => {
+        const res = await axios.get("/home/groups")
+        console.log(res);
+        // this.setState({ navs: res.data.body })
     }
 
     render() {
@@ -55,6 +75,16 @@ class Index extends Component {
                         ))}
                     </Carousel>}
                 </div>
+                {/* 2 首页导航 开始 */}
+                <div className={indexCss.index_nav}>
+                    {this.state.navs.map(v => <div className={indexCss.nav_item}
+                        key={v.id}
+                    >
+                        <img src={v.imgSrc} alt="" />
+                        <p>{v.text}</p>
+                    </div>)}
+                </div>
+                {/* 2 首页导航 结束 */}
             </div>
         );
     }
